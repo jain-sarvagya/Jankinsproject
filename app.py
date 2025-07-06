412d46a70a80d967f6c55983dfbce72a5c69abd5
jain-sarvagya <jain.sarvagya.001@gmail.com>
2025-07-06 09:51:54 +0530

    Update message

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, CI/CD with Jenkins!"
-
+    return "Namesta, CI/CD with Jenkins!"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
